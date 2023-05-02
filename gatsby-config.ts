13d75d812f59f5b10bf0bab64622c30316fef453
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-02 17:40:00 +0200

    added favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,16 +12,18 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/logo.png"
     }
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/",
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+
+  ]
 };
 
 export default config;
